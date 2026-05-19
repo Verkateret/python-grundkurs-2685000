bffbbbb531ec2df21be40310e4330580e0cd176b
Verkateret <ramon.stadelmann96@gmail.com>
2026-05-19 08:53:22 +0000

    Übung gemacht

---
+++ beispiel_code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#Rechenoperationen mit Python
+a=10
+b = 5
+c = 3
+
+# Addition
+summe = a + b
+print("Summe: ", summe)
+
+# Subtraktion
+differenz = a - b
+print("Differenz: ", differenz)
+
+# Multiplikation
+produkt= a*b
+print("Produkt: ", produkt)
+
+# Division
+quotient=a/b
+print("Quotient: ", quotient)
+
+# Ganzzahlige Division
+ganzzahl_quotient= a // c
+print("Ganzzahliger Quotient: ", ganzzahl_quotient)
+
