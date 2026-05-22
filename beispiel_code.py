ea27a4e3d9030db87e5f66bdd482d670dfe74fcc
Verkateret <ramon.stadelmann96@gmail.com>
2026-05-22 12:10:06 +0000

    Zwischenspeicher des Fortschritts

---
+++ beispiel_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Schleifen in Python
+
+# for-Schleife: Iteration über eine Liste von Zahlen
+zahlen = [1, 2, 3, 4, 5]
+for zahl in zahlen:
+  print("Zahl aus der Liste:",zahl)
+
+# for-Schleife: Iteration über einen Bereich (range)
+for i in range(1, 6):
+  print("Aktueller Wert von i:",zahlen[(i-1)])
