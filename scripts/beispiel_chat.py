9b6e250238ed9b7499abadaddc829d1f2fb0d1e6
VERTIGIS\r.stadelmann <ramon.stadelmann@vertigis.com>
2026-05-29 09:45:36 +0200

    funktion addier_zahlen mit AI generiert

---
+++ scripts/beispiel_chat.py
@@ -0,0 +1,17 @@
+def addiere_zahlen(zahl1, zahl2):
+    """Addiere zwei Zahlen und gib die Summe zurück.
+
+    Parameter:
+    zahl1 (int | float): Die erste Zahl.
+    zahl2 (int | float): Die zweite Zahl.
+
+    Rückgabe:
+    int | float: Die Summe von zahl1 und zahl2.
+    """
+    return zahl1 + zahl2
+
+resultat = addiere_zahlen(5, 10)
+print(resultat) # Ausgabe: 15
+
+resultat = addiere_zahlen(-3, 7)
+print(resultat)  # Ausgabe: 4
